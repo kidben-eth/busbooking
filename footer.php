63a913b9f4eb9de632b76d22c0838426757a9c79
unigwe Benjamin Ifechukwu <97415023+kidben-eth@users.noreply.github.com>
2023-04-19 22:36:34 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer>
+        <div class="footerInfo">
+            <div class="footerContainer">
+                <div class="footerItem">
+
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Follow Us <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <div class="socialLink facebook">
+                                <i class="fab fa-facebook-f"></i>
+                            </div>
+                            <div class="socialLink instagram">
+                                <i href class="fab fa-instagram"></i>
+                            </div>
+                            <div class="socialLink twitter">
+                                <i class="fab fa-twitter"></i>
+                            </div>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Service <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Emergency </a>
+                            <a href="#"> check up</a>
+                            <a href="#"> Lost Items </a>
+                            <a href="#"> Details </a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Our Health <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> V I P </a>
+                            <a href="#"> Drivers</a>
+                            <a href="#"> Partner</a>
+                            <a href="#"> Veichel</a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Address <span></span></h4>
+                        </div>
+                        <h5 class="addressTitle">Don Bosco Institute of Technology</h5>
+                        <div class="callInfo">
+                            <h6>Call</h6> <i class="fas fa-arrow-right"></i>
+                            <span>+2348107338821</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
